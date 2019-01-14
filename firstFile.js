427c58225cad6c5b36c072aa58b81237385a222e
Robert Bunch <rdbunch@gmail.com>
2019-01-14 12:36:51 -0500

    init repo

---
+++ firstFile.js
@@ -0,0 +1,35 @@
+// // Native elements
+// const x = 1;
+// console.log(x);
+// let atlTeams = [
+//     'Braves',
+//     'Falcons',
+//     'Hawks',
+//     'United'
+// ]
+
+
+// atlTeams = {
+//     fball: 'Falcons',
+//     bball: 'Hawks',
+//     baseball: 'Braves',
+//     foooootball: 'United'
+// }
+// console.log(atlTeams.fball)
+// // console.log(window);
+// // alert("I'm in the browser!")
+// // document.querySelector('body');
+
+// Like python, you can fetch modules. 
+// import DateTime
+// pip install pygame
+// import pygame
+
+const os = require('os');
+// console.log(os)
+const cpus = os.cpus();
+console.log(cpus);
+const freeMem = os.freemem();
+console.log(freeMem);
+const nI = os.networkInterfaces();
+console.log(nI);
